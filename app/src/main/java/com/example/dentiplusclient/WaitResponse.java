0750f27179fd6d8ab7bd034657c7a0278d4646a1
basselnole <bassel.sergawe@gmail.com>
2020-12-14 10:26:45 +0100

    ok

--- app/src/main/java/com/example/dentiplusclient/WaitResponse.java
+++ app/src/main/java/com/example/dentiplusclient/WaitResponse.java
@@ -3,8 +3,11 @@ package com.example.dentiplusclient;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,10 +24,32 @@ public class WaitResponse extends AppCompatActivity {
     private String request_key="request_key";
     private String key,request_status;
 
+    private AlertDialog.Builder builder;
+
+    private void delete_request(final String parent){
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Requests");
+        reference.child(parent).removeValue();
+        
+       /*
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                reference.child(parent).removeValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        */
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait_response);
+
+        builder = new AlertDialog.Builder(this);
 
         // to know when status changes
         Intent intent = getIntent();
@@ -37,12 +62,41 @@ public class WaitResponse extends AppCompatActivity {
                     request_key = dataSnapshot.getValue().toString();
 
                     if (request_key.equals("1")) { // request accepted
+
                         Toast.makeText(WaitResponse.this, "Appointment Confirmed", Toast.LENGTH_SHORT).show();
-                        
+
+                        builder.setMessage("Appointment Confirmed")
+                                .setCancelable(false)
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        delete_request(request_key);
+                                        //finish();
+                                    }
+                                });
+                        //Creating dialog box
+                        AlertDialog alert = builder.create();
+                        alert.setTitle("Appointment Confirmed");
+                        alert.show();
+
                     }else if(request_key.equals("2")){ //pending
                         Toast.makeText(WaitResponse.this, "pending", Toast.LENGTH_SHORT).show();
                     }else if(request_key.equals("0")){ //declined
-                         Toast.makeText(WaitResponse.this, "declined", Toast.LENGTH_SHORT).show();
+                         Toast.makeText(WaitResponse.this, "Appointment Declined", Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(WaitResponse.this, "Appointment Declined", Toast.LENGTH_SHORT).show();
+
+                        builder.setMessage("Appointment Declined")
+                                .setCancelable(false)
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        delete_request(request_key);
+                                        //finish();
+                                    }
+                                });
+                        //Creating dialog box
+                        AlertDialog alert = builder.create();
+                        alert.setTitle("Appointment Declined");
+                        alert.show();
                     }
             }
             @Override
